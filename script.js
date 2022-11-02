12932ba4a3e7895793c0c2ac05b6e838b37131d6
Sean Beaudoin <s10_n@mailbox.org>
2022-11-01 23:14:43 -0700

    Write basic math functions

---
+++ script.js
@@ -0,0 +1,21 @@
+// Basic math operation functions:
+
+// Addition
+function add(addendA, addendB) {
+    return addendA + addendB;
+}
+
+// Subtraction
+function subtract(minuend, subtrahend) {
+    return minuend - subtrahend;
+}
+
+// Multiplication
+function multiply(multiplicand, multiplier) {
+    return multiplicand * multiplier;
+}
+
+// Division
+function divide(dividend, divisor) {
+    return dividend / divisor;
+}
